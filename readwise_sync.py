25345c42dc37f62b187c756b1f1d4033e2e8235e
Manan Khattar <mkhattar@berkeley.edu>
2023-07-03 01:45:41 -0700

    don't need book api anymore

--- readwise_sync.py
+++ readwise_sync.py
@@ -7,45 +7,28 @@ import pytz
 readwise_token = "3lZVLq2HLf27gLCVqTzVTmvlTcmHMXkdxuEeqKraJtHxoYCyyJ"
 zettel_folder = os.path.expanduser("~/Dropbox/zettel")
 
-book_id_to_metadata = {}
 
-
-def fetch_from_highlights_api(updated_after=None):
+def fetch_from_export_api(updated_after=None):
     full_data = []
-    current_page = 1
+    next_page_cursor = None
     while True:
         params = {}
-        params["page"] = current_page
+        if next_page_cursor:
+            params["pageCursor"] = next_page_cursor
         if updated_after:
-            params["highlighted_at__gt"] = updated_after
+            params["updatedAfter"] = updated_after
+        print("Making export api request with params " + str(params) + "...")
         response = requests.get(
-            url="https://readwise.io/api/v2/highlights/",
+            url="https://readwise.io/api/v2/export/",
             params=params,
             headers={"Authorization": f"Token {readwise_token}"},
             verify=False,
         )
-        response_json = response.json()
-        full_data.extend(response_json["results"])
-        if response_json["next"]:
-            current_page += 1
-        else:
+        full_data.extend(response.json()["results"])
+        next_page_cursor = response.json().get("nextPageCursor")
+        if not next_page_cursor:
             break
     return full_data
-
-
-def fetch_book_metadata(book_id):
-    if book_id in book_id_to_metadata:
-        return book_id_to_metadata[book_id]
-    response = requests.get(
-        url=f"https://readwise.io/api/v2/books/{book_id}/",
-        headers={"Authorization": f"Token {readwise_token}"},
-    )
-    response_json = response.json()
-    # return {
-    #     "readable_title": response_json["title"],
-    # }
-    book_id_to_metadata[book_id] = response_json
-    return response_json
 
 
 def populate_new_article_template(title, source_url, highlighter_url):
@@ -111,22 +94,21 @@ def add_highlight_to_zettel(file_path, highlight_text, comment_text):
 
 if __name__ == "__main__":
     last_fetch_was_at = datetime.datetime.now(pytz.utc) - datetime.timedelta(
-        minutes=60
-    )  # use your own stored date
-    new_data = fetch_from_highlights_api(last_fetch_was_at.isoformat())
+        seconds=20
+    )
+    print(last_fetch_was_at.isoformat())
+    new_data = fetch_from_export_api(last_fetch_was_at.isoformat())
     print(json.dumps(new_data, indent=4))
     for idx, book in enumerate(new_data):
-        book_id = book["book_id"]
-        book_details = fetch_book_metadata(book_id)
-        print(book_details)
-        # source_url = book["source_url"]
-        # highlighter_url = book["unique_url"]
-        # file_path = does_file_exist_in_zettel_folder(book_title)
-        # if not file_path:
-        #     file_path = create_zettel(
-        #         book_title, source_url, highlighter_url, idx
-        #     )
-        # for highlight in book["highlights"]:
-        #     text = highlight["text"]
-        #     comment = highlight["note"]
-        #     add_highlight_to_zettel(file_path, text, comment)
+        book_title = book["title"]
+        source_url = book["source_url"]
+        highlighter_url = book["unique_url"]
+        file_path = does_file_exist_in_zettel_folder(book_title)
+        if not file_path:
+            file_path = create_zettel(
+                book_title, source_url, highlighter_url, idx
+            )
+        for highlight in book["highlights"]:
+            text = highlight["text"]
+            comment = highlight["note"]
+            add_highlight_to_zettel(file_path, text, comment)
